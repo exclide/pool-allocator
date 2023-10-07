bb6ef901a49d2abc2c2a3cd5ccdc2c01ffa345c5
exclide <exclide1@gmail.com>
2023-10-07 23:06:06 +0300

    improve spinlock

--- spinlock_mutex.h
+++ spinlock_mutex.h
@@ -12,12 +12,22 @@ private:
     std::atomic_flag flag = ATOMIC_FLAG_INIT;
 public:
     void lock() {
-        while (flag.test_and_set()); //spin
+        while (flag.test_and_set(std::memory_order_acquire)) {
+            __builtin_ia32_pause();
+        } //spin
     }
 
     void unlock() {
-        flag.clear();
+        flag.clear(std::memory_order_release);
     }
 };
+
+static inline void spin_pause() {
+#if defined(__GNUC__) || defined(__GNUG__)
+    __builtin_ia32_pause();
+#elif defined(_MSC_VER)
+    _mm_pause();
+#endif
+}
 
 #endif //LOCKFREE_SPINLOCK_MUTEX_H
